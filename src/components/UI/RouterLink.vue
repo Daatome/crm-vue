ffad0dcc8ee2e1efc10f625f3dbc6162e9f66f3a
Daniel Atonal <danielatonal8@gmail.com>
2023-08-18 23:55:42 -0600

    heading template created

---
+++ src/components/UI/RouterLink.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+    import { RouterLink } from 'vue-router';
+
+
+    defineProps({
+        to:{
+            type:String
+        }
+    })
+</script>
+<template>
+    <RouterLink :to="{name: to}" class="block md:inline-block bg-blue-600 py-2 px-3 rounded-lg text-white font-bold hover:text-gray-100 hover:bg-blue-500">
+        <slot></slot>
+    </RouterLink>
+</template>
